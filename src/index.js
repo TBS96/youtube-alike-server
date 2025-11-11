f72e2ba6780fa9d2bf1166f123c4b644d734d7b2
TBS96 <9tbs6@proton.me>
2025-11-11 21:34:35 +0530

    minor changes
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 /* SECOND APPROACH (MODULAR) */
 
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import app from "./app.js";
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config({
-  path: "./.env",
+  path: './.env',
 });
 
 connectDB()
   .then(() => {
     app.on('error', (error) => {
-        console.error(`APP ERROR: ${error}`);
-    })
+      console.error(`APP ERROR: ${error}`);
+    });
 
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at PORT: ${process.env.PORT}`);
-    })
+    });
   })
   .catch((err) => {
     console.error(`MONGODB CONNECTION FAILED!!!: ${err}`);
   });
-
 
 
 
