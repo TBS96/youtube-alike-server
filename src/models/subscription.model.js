9f6bb1faedb9053fce62a546f32898a0111f1a3f
TBS96 <9tbs6@proton.me>
2025-12-10 18:26:51 +0530

    added notes in subsription model
    - to find subscribers of a channel, find and select the 'CHANNEL value' from the subscription model + from the documents created. DONT SELECT THE USERS.
    - to find how many channels a user has subscribed, select 'SUBSCRIBER' value from schema
    - just make sure that 'subscribers are received from channels' and 'channels are received from subscribers'
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -17,3 +17,15 @@ const subscriptionSchema = new Schema(
 );
 
 export const Subscription = model('Subscription', subscriptionSchema);
+
+
+
+
+
+
+// NOTE:
+// to find subscribers of a channel, find and select the 'CHANNEL value' from the subscription model + from the documents created. DONT SELECT THE USERS.
+
+// to find how many channels a user has subscribed, select 'SUBSCRIBER' value from schema
+
+// just make sure that 'subscribers are received from channels' and 'channels are received from subscribers'
