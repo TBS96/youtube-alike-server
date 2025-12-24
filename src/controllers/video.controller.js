201273c57ec741fc34f3b70807e0a8e5685c6f55
TBS96 <9tbs6@proton.me>
2025-12-24 23:56:27 +0530

    test push 2
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    // TODO: get all videos based on query, sort, pagination    
+    // TODO: get all videos based on query, sort, pagination.
     const { page = 1, limit = 10, query, sortBy = 'createdAt', sortType = 'desc', userId } = req.query;
 });
 
