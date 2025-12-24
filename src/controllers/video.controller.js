73fb37bc68138dcaae749598567a243ae828c585
TBS96 <9tbs6@proton.me>
2025-12-24 22:53:31 +0530

    destructure props from `req.query`
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+
 const getAllVideos = asyncHandler(async (req, res) => {
     // TODO: get all videos based on query, sort, pagination    
+    const { page = 1, limit = 10, query, sortBy = 'createdAt', sortType = 'desc', userId } = req.query;
 });
 
 export {
