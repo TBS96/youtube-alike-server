69b40b214cd530ddeac66e94861e4872525ee803
TBS96 <9tbs6@proton.me>
2026-01-04 19:09:47 +0530

    create `post` endpoint for `toggleTweetLike` in `like.routes.js`
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { verifyJWT } from '../middlewares/auth.middleware.js';
-import { toggleCommentLike, toggleVideoLike } from '../controllers/like.controller.js';
+import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controllers/like.controller.js';
 
 const likeRouter = Router();
 
@@ -8,5 +8,6 @@ likeRouter.use(verifyJWT);
 
 likeRouter.route('/toggle-video-like/:videoId').post(toggleVideoLike);
 likeRouter.route('/toggle-comment-like/:commentId').post(toggleCommentLike);
+likeRouter.route('/toggle-tweet-like/:tweetId').post(toggleTweetLike);
 
 export default likeRouter;
