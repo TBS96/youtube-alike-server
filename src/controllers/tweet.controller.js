6e2f8aac524eb70acb055581247701d4dd1b9810
TBS96 <9tbs6@proton.me>
2026-01-09 21:17:48 +0530

    feat: implement `getUserTweets` controller
    _**logic building phase for `getUserTweets` in `comment.controller.js`:**_
    
    1. extract `userId` from `req.params` and validate it
    2. S1 (`$match`): filter the `Tweet` collection to find documents where the `owner` field matches the `userId`
    3. S2 (`$lookup`): join with the `users` collection to retrieve the creator's `username`, `avatar`, and `fullName`
    4. S3 (`$lookup`): join with the `likes` collection to fetch all like documents associated with each tweet
    5. S4 (`$addFields`): format the output by:
    - extracting the first object from the `owner` array;
    - calculating the `likesCount` by measuring the size of the likes array;
    - determining `isLiked` by checking if the current `req.user._id` exists within the `likes.likedBy` array
    6. S5 (`$sort`): organize the result set by `createdAt` in descending order (-1) so the latest tweets appear first
    7. S6 (`$project`): remove the raw likes array from the final output to optimize the response payload
    8. return success response
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { Tweet } from '../models/tweet.model.js'
-import { isValidObjectId } from 'mongoose';
+import mongoose, { isValidObjectId } from 'mongoose';
 
 
 
@@ -165,8 +165,130 @@ const deleteTweet = asyncHandler(async (req, res) => {
     // =========== 5. return success response ===========
 });
 
+
+
+const getUserTweets = asyncHandler(async (req,res) => {
+    /* ** algorithm to follow step by step, to fetch all tweets of a specific user **
+    1. extract userId from req.params and validate it
+    2. S1 ($match): filter the Tweet collection for documents where the owner matches the userId
+    3. S2 ($lookup): join with the users collection to retrieve the creator's username, avatar, and fullName
+    4. S3 ($lookup): join with the likes collection to fetch all like records associated with each tweet
+    5. S4 ($addFields): format the output by:
+        - extracting the first object from the owner array;
+        - calculating the likesCount by measuring the size of the likes array;
+        - determining isLiked by checking if the current req.user._id exists within the likes.likedBy array
+    6. S5 ($sort): organize the result set by createdAt in descending order (-1) so the latest tweets appear first
+    7. S6 ($project): remove the raw likes array from the final output to optimize the response payload
+    8. return success response
+    */
+    
+    // ========== 1. extract userId from req.params and validate it ==========
+    const { userId } = req.params;
+    
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, 'Invalid or missing user ID');
+    }
+    // ========== 1. extract userId from req.params and validate it ==========
+
+
+    const tweets = await Tweet.aggregate([
+        // ============= 2. S1 ($match): filter the Tweet collection for documents where the owner matches the userId =============
+        {
+            $match: {
+                owner: mongoose.Types.ObjectId.createFromHexString(userId.toString())
+            }
+        },
+        // ============= 2. S1 ($match): filter the Tweet collection for documents where the owner matches the userId =============
+
+        // ========= 3. S2 ($lookup): Join with the users collection to retrieve the creator's username, avatar, and fullName =========
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'owner',
+                foreignField: '_id',
+                as: 'owner',
+                pipeline: [
+                    {
+                        $project: {
+                            fullName: 1,
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        // ========= 3. S2 ($lookup): Join with the users collection to retrieve the creator's username, avatar, and fullName =========
+
+        // =========== 4. S3 ($lookup): join with the likes collection to fetch all like records associated with each tweet ===========
+        {
+            $lookup: {
+                from: 'likes',
+                localField: '_id',
+                foreignField: 'tweet',
+                as: 'likes'
+            }
+        },
+        // =========== 4. S3 ($lookup): join with the likes collection to fetch all like records associated with each tweet ===========
+
+        // ======= 5. S4 ($addFields): format the output by extracting the first owner object and calculating the likesCount via the $size operator =======
+        {
+            $addFields: {
+                // extracting the first object from the owner array
+                owner: {
+                    $first: '$owner'
+                },
+                // extracting the first object from the owner array
+                // calculating the likesCount by measuring the size of the likes array
+                likesCount: {
+                    $size: '$likes'
+                },
+                // calculating the likesCount by measuring the size of the likes array
+                // determining isLiked by checking if the current req.user._id exists within the likes.likedBy array
+                isLiked: {
+                    $cond: {
+                        if: {$in: [req.user?._id, '$likes.likedBy']},
+                        then: true,
+                        else: false
+                    }
+                }
+                // determining isLiked by checking if the current req.user._id exists within the likes.likedBy array
+            }
+        },
+        // ======= 5. S4 ($addFields): format the output by extracting the first owner object and calculating the likesCount via the $size operator =======
+
+        // ========= 6. S5 ($sort): Organize the result set by createdAt in descending order (-1) so the latest tweets appear first =========
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        // ========= 6. S5 ($sort): Organize the result set by createdAt in descending order (-1) so the latest tweets appear first =========
+
+        // ======== 7. S6 ($project): remove the raw likes array from the final output to optimize the response payload ========
+        {
+            $project: {
+                likes: 0
+            }
+        }
+        // ======== 7. S6 ($project): remove the raw likes array from the final output to optimize the response payload ========
+    ]);
+
+    console.log('Fetched tweets: ', tweets);
+
+
+    // ============ 8. return success response ============
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweets, 'All tweets fetched, successfully')
+    );
+    // ============ 8. return success response ============
+});
+
 export {
     createTweet,
     updateTweet,
     deleteTweet,
+    getUserTweets
 }
