3db783d51555515e44d3d35e88645dc35dd6b816
TBS96 <9tbs6@proton.me>
2026-01-22 21:17:34 +0530

    create `get` endpoints for dashboard controller in `dashboard.routes.js`
    - create `get` endpoint for `getChannelStats`
    - create `get` endpoint for `getChannelVideos`
    - all above endpoints are using `verifyJWT` as middleware to prevent unauthorized access
    - configure `dashboardRouter` in `app.js`
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ import commentRouter from './routes/comment.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import playlistRouter from './routes/playlist.router.js';
+import dashboardRouter from './routes/dashboard.routes.js'
 
 // ================== routes declaration ==================
 app.use('/api/v1/users', userRouter);    // http://localhost:8000/api/v1/users/
@@ -41,6 +42,7 @@ app.use('/api/v1/comments', commentRouter); // http://localhost:8000/api/v1/comm
 app.use('/api/v1/tweets', tweetRouter);     // http://localhost:8000/api/v1/tweets/
 app.use('/api/v1/subscriptions', subscriptionRouter);     // http://localhost:8000/api/v1/subscriptions/
 app.use('/api/v1/playlists', playlistRouter);     // http://localhost:8000/api/v1/playlists/
+app.use('/api/v1/dashboard', dashboardRouter);     // http://localhost:8000/api/v1/dashboard/
 
 
 // export default app;
---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+import { getChannelStats, getChannelVideos } from '../controllers/dashboard.controller.js';
+
+const dashboardRouter = Router();
+
+dashboardRouter.use(verifyJWT);
+
+dashboardRouter.route('/stats').get(getChannelStats);
+dashboardRouter.route('/channel-videos').get(getChannelVideos);
+
+export default dashboardRouter;
